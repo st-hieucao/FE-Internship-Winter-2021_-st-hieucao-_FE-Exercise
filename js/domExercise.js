c01dc24083946b2dbedc566ced0a832029c58ca4
st-hieucao <hieu.cao@supremetech.vn>
2021-10-24 15:37:29 +0700

    check DOM loaded

--- js/domExercise.js
+++ js/domExercise.js
@@ -1,60 +1,65 @@
 // ---------------- BAI1
-var buttonElement = document.querySelector(".button");
-var alertElement = document.querySelector(".alert");
+var buttonElement;
+var alertElement;
 var age;
+window.addEventListener('DOMContentLoaded', (event) => {
+  buttonElement = document.querySelector('.button');
+  alertElement = document.querySelector('.alert');
 
-buttonElement.onclick = function (e) {
-  e.preventDefault();
-  var input = document.querySelector(".input").value;
-  var currentYear = new Date().getFullYear();
+  buttonElement.onclick = function (e) {
+    e.preventDefault();
+    var input = document.querySelector('.input').value;
+    var currentYear = new Date().getFullYear();
 
-  age = currentYear - parseInt(input);
+    age = currentYear - parseInt(input);
 
-  alertElement.innerHTML = "Tuổi của bạn là: " + age;
-};
+    alertElement.innerHTML = 'Tuổi của bạn là: ' + age;
+  };
+});
+
 
 // ---------------- BAI2
 var options = [
   {
-    title: "Basic",
-    price: "$10 / month",
+    title: 'Basic',
+    price: '$10 / month',
     listInfo: [
-      "10 users included",
-      "2 GB of storage",
-      "Email support",
-      "Help center access",
+      '10 users included',
+      '2 GB of storage',
+      'Email support',
+      'Help center access',
     ],
-    action: "Get Started",
+    action: 'Get Started',
   },
   {
-    title: "Pro",
-    price: "$30 / month",
+    title: 'Pro',
+    price: '$30 / month',
     listInfo: [
-      "100 users included",
-      "20 GB of storage",
-      "Priority email support",
-      "Help center access",
+      '100 users included',
+      '20 GB of storage',
+      'Priority email support',
+      'Help center access',
     ],
-    action: "Buy Now",
+    action: 'Buy Now',
   },
 ];
-var listOptionsElement = document.querySelector(".list-options");
+var listOptionsElement = document.querySelector('.list-options');
 
 options.forEach(function (item) {
-  var liElement = document.createElement("li");
-  var optionTitleElement = document.createElement("h3");
-  var priceElement = document.createElement("h4");
-  var listInfoElement = document.createElement("ul");
-  var buttonElement = document.createElement("button");
+  var liElement = document.createElement('li');
+  var optionTitleElement = document.createElement('h3');
+  var priceElement = document.createElement('h4');
+  var listInfoElement = document.createElement('ul');
+  var buttonElement = document.createElement('button');
 
-  liElement.setAttribute("class", "option");
-  optionTitleElement.setAttribute("class", "option-title");
-  priceElement.setAttribute("class", "price");
-  listInfoElement.setAttribute("class", "list-info");
-  if (item.title === "Basic") {
-    buttonElement.setAttribute("class", "btn btn-secondary");
+  liElement.setAttribute('class', 'option');
+  optionTitleElement.setAttribute('class', 'option-title');
+  priceElement.setAttribute('class', 'price');
+  listInfoElement.setAttribute('class', 'list-info');
+  if (item.title === 'Basic') {
+    buttonElement.setAttribute('class', 'btn btn-secondary');
   } else {
-    buttonElement.setAttribute("class", "btn btn-primary");
+    buttonElement.setAttribute('class', 'btn btn-primary');
   }
 
   optionTitleElement.innerHTML = item.title;
@@ -62,8 +67,8 @@ options.forEach(function (item) {
   buttonElement.innerHTML = item.action;
 
   item.listInfo.forEach(function (infoItem) {
-    var infoItemElement = document.createElement("li");
-    infoItemElement.setAttribute("class", "info-item");
+    var infoItemElement = document.createElement('li');
+    infoItemElement.setAttribute('class', 'info-item');
 
     infoItemElement.innerHTML = infoItem;
 
